1a5f297c763e2c3dfb96b92b555147c1ad060b3c
AdamAlJaaouni <aljaadam392@gotvdsb.ca>
2024-03-20 11:10:15 -0400

    made limelight pose only get used in tele

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -134,10 +134,12 @@ private SwerveModuleState[] getModuleStates() {
   @Override
   public void periodic() {
     // Update the odometry in the periodic block
-  //  if (m_limelightSubsystem.getId() > 0){
-  //   resetOdometry(getLimelightPose());
-  //   // setGyroYaw(m_limelightSubsystem.getBotPose().getRotation().getDegrees()); //accuracy of april tag may be worse than gyro drift
-  //  }else{
+  if (DriverStation.isTeleop() == true){
+   if (m_limelightSubsystem.getId() > 0){
+    resetOdometry(getLimelightPose());
+    // setGyroYaw(m_limelightSubsystem.getBotPose().getRotation().getDegrees()); //accuracy of april tag may be worse than gyro drift
+   }
+   }else{
       m_odometry.update(
         getWrappedHeading(),
         new SwerveModulePosition[] {
@@ -146,7 +148,7 @@ private SwerveModuleState[] getModuleStates() {
             m_rearLeft.getPosition(),
             m_rearRight.getPosition()
          });
-  //  }
+   }
 
 
   
